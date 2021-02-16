731201a762b77abfd661c018064fed28f96bac6c
disizaniknem <disizaniknem@gmail.com>
2021-02-16 15:54:58 +0100

    Login a user

--- src/Application.kt
+++ src/Application.kt
@@ -1,17 +1,11 @@
 package com.disizaniknem
 
-import com.disizaniknem.data.collections.User
-import com.disizaniknem.data.registerUser
+import com.disizaniknem.routes.loginRoute
 import com.disizaniknem.routes.registerRoute
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.gson.*
-import io.ktor.response.*
-import io.ktor.request.*
 import io.ktor.routing.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -22,6 +16,7 @@ fun Application.module(testing: Boolean = false) {
     install(CallLogging)
     install(Routing) {
         registerRoute()
+        loginRoute()
     }
     install(ContentNegotiation) {
         gson {
